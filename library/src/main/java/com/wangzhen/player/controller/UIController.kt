ae1cedb1ba474efa33851c73e6d4913f3891e741
wangzhen <lavalike@yeah.net>
2020-12-09 17:08:08 +0800

    播放状态UI自动隐藏逻辑

--- library/src/main/java/com/wangzhen/player/controller/UIController.kt
+++ library/src/main/java/com/wangzhen/player/controller/UIController.kt
@@ -1,11 +1,12 @@
 package com.wangzhen.player.controller
 
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import com.google.android.exoplayer2.ExoPlaybackException
 import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.video.VideoListener
 import com.wangzhen.player.R
 import com.wangzhen.player.type.PlayerState
 
@@ -15,37 +16,47 @@ import com.wangzhen.player.type.PlayerState
  */
 class UIController(private val container: FrameLayout) : Controller() {
     private var isPlaying: Boolean = false
-    private lateinit var btnPlayPause: ImageView
+    private lateinit var rootView: View
+    private lateinit var containerPlaying: View
     private lateinit var btnReplay: ImageView
     private lateinit var bufferLoading: View
     private lateinit var btnRetry: View
 
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun run() {
-        val view = View.inflate(container.context, R.layout.player_ui_controller_layout, null)
-        container.addView(view)
-        initViews(view)
-        hideAll()
-        updateState(PlayerState.BUFFERING)
+        rootView = View.inflate(container.context, R.layout.player_ui_controller_layout, null)
+        container.addView(rootView)
+        initViews(rootView)
         playerView?.player?.let { player ->
             player.removeListener(eventListener)
             player.addListener(eventListener)
-            player.removeVideoListener(videoListener)
-            player.addVideoListener(videoListener)
         }
     }
 
     private fun initViews(view: View) {
-        view.findViewById<ImageView>(R.id.btn_play_pause).apply {
-            btnPlayPause = this
-            setOnClickListener {
-                if (isPlaying) {
-                    playerView?.pause()
-                    setImageResource(R.mipmap.module_player_controls_play)
-                } else {
-                    playerView?.resume()
-                    setImageResource(R.mipmap.module_player_controls_pause)
+        view.setOnClickListener {
+            if (isPlaying) {
+                containerPlaying.visibility = if (containerPlaying.visibility == View.VISIBLE)
+                    View.GONE
+                else
+                    View.VISIBLE
+                checkScheduleDisappear()
+            }
+        }
+        containerPlaying = view.findViewById<View>(R.id.container_playing).apply {
+            findViewById<ImageView>(R.id.btn_play_pause).apply {
+                setOnClickListener {
+                    playerView?.player?.let { player ->
+                        if (player.playWhenReady) {
+                            playerView?.pause()
+                            setImageResource(R.mipmap.module_player_controls_play)
+                        } else {
+                            playerView?.resume()
+                            setImageResource(R.mipmap.module_player_controls_pause)
+                        }
+                    }
                 }
-                isPlaying = !isPlaying
             }
         }
         view.findViewById<ImageView>(R.id.btn_replay).apply {
@@ -66,34 +77,60 @@ class UIController(private val container: FrameLayout) : Controller() {
             }
         }
         view.addOnAttachStateChangeListener(lifecycleListener)
-        updateState(Player.STATE_BUFFERING)
+
+        hideAll()
+        updateState(PlayerState.BUFFERING)
     }
 
     private fun updateState(state: Int) {
         hideAll()
+        isPlaying = false
         when (state) {
             PlayerState.BUFFERING -> {
                 bufferLoading.visibility = View.VISIBLE
             }
             PlayerState.READY -> {
-                btnPlayPause.visibility = View.VISIBLE
+                isPlaying = true
+                containerPlaying.visibility = View.VISIBLE
+                checkScheduleDisappear()
             }
             PlayerState.END -> {
-                isPlaying = false
                 btnReplay.visibility = View.VISIBLE
             }
             PlayerState.ERROR -> {
-                isPlaying = false
                 btnRetry.visibility = View.VISIBLE
             }
         }
     }
 
+    private fun checkScheduleDisappear() {
+        playerView?.player?.let {
+            if (it.playWhenReady) {
+                scheduleDisappear()
+            } else {
+                unScheduleDisappear()
+            }
+        }
+    }
+
+    private fun unScheduleDisappear() {
+        handler.removeCallbacks(disappearRunnable)
+    }
+
+    private fun scheduleDisappear() {
+        unScheduleDisappear()
+        handler.postDelayed(disappearRunnable, 2000L)
+    }
+
     private fun hideAll() {
         bufferLoading.visibility = View.GONE
-        btnPlayPause.visibility = View.GONE
+        containerPlaying.visibility = View.GONE
         btnReplay.visibility = View.GONE
         btnRetry.visibility = View.GONE
+    }
+
+    private val disappearRunnable = Runnable {
+        containerPlaying.visibility = View.GONE
     }
 
     private val lifecycleListener = object : View.OnAttachStateChangeListener {
@@ -102,6 +139,7 @@ class UIController(private val container: FrameLayout) : Controller() {
         }
 
         override fun onViewDetachedFromWindow(v: View) {
+            unScheduleDisappear()
             playerView?.stop()
         }
 
@@ -120,12 +158,6 @@ class UIController(private val container: FrameLayout) : Controller() {
         override fun onPlayerError(error: ExoPlaybackException?) {
             super.onPlayerError(error)
             updateState(PlayerState.ERROR)
-        }
-    }
-
-    private val videoListener = object : VideoListener {
-        override fun onRenderedFirstFrame() {
-            isPlaying = true
         }
     }
 }
